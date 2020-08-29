4b471ddfa444763fbcb6232f921e074bafa050aa
Francisco Garcia <garciafran@hotmail.com>
2020-08-28 19:27:22 -0500

    Obteniendo los link de los articulos con Python

--- scraper.py
+++ scraper.py
@@ -2,17 +2,17 @@ import requests
 import lxml.html as html
 
 HOME_URL = 'https://www.larepublica.co/'
-
 XPATH_LINK_TO_ARTICLE = '//h2/a[@class="empresasSect"]/@href'
 XPATH_TITLE = '//h2/a[@class="finanzasSect"]/text()'
 XPATH_SUMMARY = '//div[@class="lead"]/p/text()'
 XPATH_BODY = '//div[@class="html-content"]/p[not(@class)]/text()'
 
+
 def parse_home():
     try:
         response = requests.get(HOME_URL)
         if response.status_code == 200:
-            home = response.content.decod('utf-8')
+            home = response.content.decode('utf-8')
             parsed = html.fromstring(home)
             links_to_notices = parsed.xpath(XPATH_LINK_TO_ARTICLE)
             print(links_to_notices)
@@ -21,9 +21,9 @@ def parse_home():
     except ValueError as ve:
         print(ve)
 
+
 def run():
     parse_home()
-
 
 
 if __name__ == "__main__":
